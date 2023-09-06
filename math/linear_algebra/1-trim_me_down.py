2b95cc7dae06aff6701aa764fe3712a4e2f4881c
kakarbo <alexcarbo8809@gmail.com>
2023-09-06 11:24:25 -0500

    accediendo a los elementos 3 y 4 de la columna de una matriz de forma tradiconal en python

---
+++ math/linear_algebra/1-trim_me_down.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+"""
+
+matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = []
+for vector in matrix:
+    new_vector = []
+    for element in range(len(vector)):
+        if element == 2 or element == 3:
+            new_vector.append(vector[element])
+    the_middle.append(new_vector)
+print("The middle columns of the matrix are: {}".format(the_middle))
