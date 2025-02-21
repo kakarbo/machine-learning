c3b3c4d7ba7678aac90c4b21c34d41d07850c1ac
kakarbo <alexcarbo8809@gmail.com>
2025-02-21 11:42:45 +0000

    Possion PMF and Possion CDF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+'''
+The probability module is a branch of mathematics that studies uncertainty and measures the likelihood of an event occurring. It is represented by a number between 0 and 1:
+
+* 0 means the event will never occur.
+* 1 means the event will always occur.
+* An intermediate value indicates the probability of occurrence.
+'''
 
 class Poisson:
     """
@@ -10,6 +17,7 @@ class Poisson:
     def __init__(self, data=None, lambtha=1.):
         """contructor
         """
+        self.data = data
         if data is None:
             self.lambtha = lambtha
             if lambtha <= 0:
@@ -21,4 +29,40 @@ class Poisson:
                 self.lambtha = sum(data) / len(data)
             else:
                 raise TypeError("data must be a list")
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of 'successes'
+        """
+
+        if isinstance(k, int):
+            if k < 0:
+                return 0
+        else:
+            k = int(k)
+        fact = 1
+        for i in range(1, k+1):
+            fact = fact * i
+        print(self.lambtha)
+        k = ((self.lambtha**k)*(self.e**self.lambtha))/fact
+        return k
+
+
+
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of 'successes'
+        """
+        count = 0.0
+        if isinstance(k, int):
+            if k < 0:
+                return 0
+        else:
+            k = int(k)
+        
+        for value in self.data:
+            if value <= k:
+                count += 1
+        k = count / len(self.data)
+        return k
 
