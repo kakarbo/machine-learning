b089fd882cc895437352d6f4633d74331d37649e
kakarbo <alexcarbo8809@gmail.com>
2025-03-11 11:03:22 +0000

    Privatize Neuron

---
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+Neuron =  __import__('0-neuron').Neuron
+import numpy as np
+"""
+Binary Classification
+"""
+
+class Neuron(Neuron):
+    """
+    Defined a single neuron performing binary classification
+    """
+    __W = np.random.randn(1, 784)
+    __b = 0
+    __A = 0
+    def __init__(self, nx):
+        """
+        class constructor
+        """
+        super().__init__(nx)
+
