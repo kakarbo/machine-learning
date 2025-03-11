31ecc3a57a564d9e1ce82a086dde31be2b95ffa8
kakarbo <alexcarbo8809@gmail.com>
2025-03-11 11:04:19 +0000

    Neuron cost

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Neuron Cost
+"""
+Nueron = __import__("2-neuron").Neuron
+
+class Neuron(Neuron):
+    """
+    Class Neuron
+    """
+
+    def cost(slef, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        """
+
