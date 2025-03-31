2c14165e50f3e1a83e9f6945d0e959057436d92e
Cristhian Carbonell <alexcarbo8809@gmail.com>
2025-03-31 12:49:43 +0000

    create placeholders

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 """
-
+In TensorFlow 1.x, placeholders were a mechanism to define
+inputs to a computational graph. They acted as "promises"
+to provide data later, during the execution of the graph
+via a Session. However, TensorFlow 2.x deprecated
+placeholders in favor of a more intuitive approach
+(eager execution and tf.function). Here's a detailed
+breakdown
 """
 import tensorflow.compat.v1 as tf
 
@@ -13,7 +19,7 @@ def create_placeholders(nx, classes):
         classes (float): the number of classes in our classifier
 
     Returns:
-        (x, y) (tupla): returns two placeholders x and y,
+        (x, y) (tupla(Tensor, Tensor)): returns two placeholders x and y,
         for the neural network
     """
 
@@ -26,8 +32,8 @@ def create_placeholders(nx, classes):
     with graph.as_default():
 
         # creating a placeholder
-        x = tf.placeholder(tf.float64, (, nx), name="x")
-        y = tf.placeholder(tf.float64, (, classes), name="y")
+        x = tf.placeholder(tf.float64, (0, nx), name="x")
+        y = tf.placeholder(tf.float64, (0, classes), name="y")
 
         return (x, y)
 
