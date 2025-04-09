f9e321f6e7e4c611b8babe668c95dc1679a7d7ad
Cristhian Carbonell <alexcarbo8809@gmail.com>
2025-04-09 11:18:00 +0000

    init function calculate loss

---
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Loss
+"""
+
+def calculate_loss(y, y_pred):
+    """
+    Calculate loss
+
+    Parameters:
+        y (placeholder): is a placeholder for the labels
+        of the input data
+        y_pred (tensor): is a tensor containing the 
+        network's predictions
+
+    Returns:
+        Tensor: a tensor containing the loss of the prediction
+    """
