df6d847d85713ff0de1cde1ee9c9c668162a8913
Cristhian Carbonell <alexcarbo8809@gmail.com>
2025-05-06 11:04:32 +0000

    Momemtum Upgraded

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Momentum upgraded
+"""
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    Creates the training operation for neural network in tensorflow using the
+    gradiente descent with momentum optimization algorithm
+
+    Parameters:
+        loss () -
+        alpha () -
+        beta1 () - 
+
+    Returns:
+        
+    """
