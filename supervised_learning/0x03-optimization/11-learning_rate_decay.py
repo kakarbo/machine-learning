bbe23c0ea49cc2b16887f55b91656d4885c69a8a
Cristhian Carbonell <alexcarbo8809@gmail.com>
2025-05-12 13:06:27 +0000

    learning rate decay

---
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Learning rate decay
+"""
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Updates the learning rate using inverse time decay in numpy.
+
+    Parameters:
+        alpha (float): is the original learning rate
+        decay_rate (int): is the weight used to determine the rate at which alpha
+        will decay
+        global_step (int): is the number of passes of gradient descent that have
+        elapsed.
+        decay_step (int): is the number of passes of gradient descent that should
+        occur before alpha is decayed further.
+
+    Returns:
+        the updated value for alpha
+    """
+    decayed_alpha = alpha / (1 + decay_rate * (global_step / decay_rate))
+
+    return decayed_alpha
